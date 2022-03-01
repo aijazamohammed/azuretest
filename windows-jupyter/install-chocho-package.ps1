26c28ff5f22b68f6e3dfa926e807d24211d407be
Aijaz Mohammed <aijaza.mohammed@hcl.com>
2022-03-01 12:14:25 +0000

    Addition of Windows Repo

---
+++ windows-jupyter/install-chocho-package.ps1
@@ -0,0 +1,6 @@
+#instead of useing Jupyter Use Google Collab
+#there you don't even have to install anything
+#GOTo : https://colab.research.google.com/
+
+#but if you want  Jupyter only use
+pip install jupyter
