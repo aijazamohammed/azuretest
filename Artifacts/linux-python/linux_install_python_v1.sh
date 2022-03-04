20148ef8651f85c94da4277f37ed89de95f0bb6b
Aijaz Mohammed <aijaza.mohammed@hcl.com>
2022-03-04 04:06:11 +0000

    test

--- Artifacts/linux-python/linux_install_python_v1.sh
+++ Artifacts/linux-python/linux_install_python_v1.sh
@@ -1,34 +1,9 @@
-# Script to install PowerShell on Linux package on Azure DevTest lab Linux VMs.
-#
-# NOTE: Intended for use by the Azure DevTest Lab artifact system.
-#
-# Usage: 
-#
-# linux-powershell.sh PACKAGE-URL
-#
-
 #!/bin/bash
 
-isApt=`command -v apt-get`
-isYum=`command -v yum`
+# Check if docker is already installed.
+python --version
+installationStatus=$(echo $?)
 
-set -e
-cd /tmp
-URL=$1
-FILE_NAME="${URL##*/}"
-echo "Downloading $FILE_NAME"
-wget $URL
-
-if [ -n "$isYum" ] ; then
-    echo "Using yum package manager"
-    yum install -y python3
-elif [ -n "$isApt" ] ; then
-    echo "Using apt package manager"
-    if [ $(lsb_release -r -s) = "16.04" ]; then
-        apt-get -q -y install python
-        dpkg -i $FILE_NAME
-    elif [ $(lsb_release -r -s) = "14.04" ]; then
-        apt-get -q -y install python3*
-        dpkg -i $FILE_NAME
-    fi
+if [ $installationStatus -eq 127 ] ; then
+    yum install -y python*
 fi
