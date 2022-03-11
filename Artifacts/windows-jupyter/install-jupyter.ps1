b01fc5b8fd4a35a127d22b06a5ecc0007209ce2c
aijazamohammed <100514629+aijazamohammed@users.noreply.github.com>
2022-03-10 23:31:24 -0500

    create new file with same content

---
+++ Artifacts/windows-jupyter/install-jupyter.ps1
@@ -0,0 +1,15 @@
+#instead of useing Jupyter Use Google Collab
+#there you don't even have to install anything
+#GOTo : https://colab.research.google.com/
+
+#but if you want Jupyter only use
+#Upgrade the Python with latest python PIP packages
+
+python -m pip install --upgrade pip
+
+#Install Jupyter using pip packages
+
+pip3.7.exe install jupyter
+
+#Run the jupyter notebook packages
+jupyter-notebook.exe
