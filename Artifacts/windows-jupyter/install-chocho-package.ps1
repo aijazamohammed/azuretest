f6350afba765b59756071c48959b2e3f674a7a15
aijazamohammed <100514629+aijazamohammed@users.noreply.github.com>
2022-03-10 23:15:56 -0500

    update master file.

--- Artifacts/windows-jupyter/install-chocho-package.ps1
+++ Artifacts/windows-jupyter/install-chocho-package.ps1
@@ -2,6 +2,14 @@
 #there you don't even have to install anything
 #GOTo : https://colab.research.google.com/
 
-#but if you want  Jupyter only use
+#but if you want Jupyter only use
+#Upgrade the Python with latest python PIP packages
+
+python -m pip install --upgrade pip
+
+#Install Jupyter using pip packages
+
 pip3.7.exe install jupyter
-jupyter notebook
+
+#Run the jupyter notebook packages
+jupyter-notebook.exe
